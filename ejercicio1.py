5d1fadd312546506d01d80284247f5d22e59bd73
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2024-03-11 20:23:09 -0500

    Desarrollo de ejercicios 1, 2 y 3

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,9 @@
 class Coche:
     def __init__(self, marca, modelo):
         # Asigna la marca proporcionada al atributo 'marca'
+        self.marca = marca
         # Asigna el modelo proporcionado al atributo 'modelo'
+        self.modelo = modelo
 
     def conducir(self):
         return f"Conduciendo el {self.marca} {self.modelo}"
